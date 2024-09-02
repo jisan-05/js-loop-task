3e5d138996627f54a6454a347916d3a3d686bc4d
Tanvir <zishantanvir05@gmail.com>
2024-09-02 10:33:45 +0600

    add sum of even num

--- sum-of.js
+++ sum-of.js
@@ -5,4 +5,15 @@ while(i <= 131){
     sum = sum + i;
     i = i+2;
 }
-console.log(sum)
+console.log("sum of odd number", sum)
+
+
+// sum of all even number from 206 to 311
+let n = 206;
+sum = 0;
+while(n <= 311){
+    sum = sum + n;
+    n = n + 2;
+
+}
+console.log("sum of even number", sum)
