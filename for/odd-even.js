4bfdffb72208488fd1132cf440bf438333a94636
Tanvir <zishantanvir05@gmail.com>
2024-09-02 11:08:03 +0600

    sum of all even or odd programm

--- for/odd-even.js
+++ for/odd-even.js
@@ -2,6 +2,7 @@ for(let i = 61; i<=100; i= i + 2){
     console.log("Odd num - ", i)
 }
 
+// even number find 
 for (let even = 78; even<= 98; even++){
     if(even % 2 === 0){
         console.log("even num - ", even);
---
+++ for/sum-of-even.js
@@ -0,0 +1,17 @@
+sum = 0;
+for(let i = 91; i<= 129; i++ ){
+    if(i % 2 === 1){
+        sum = sum + i;
+    }
+}
+console.log("sum of all odd number - ", sum)
+
+
+// Sum of all the even numbers 
+sum2 = 0;
+for(let n = 51; n<= 85; n++){
+    if(n % 2 === 0){
+        sum2 = sum2 + n;
+    }
+}
+console.log("sum of even number", sum2)
