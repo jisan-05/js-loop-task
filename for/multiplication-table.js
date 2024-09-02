8d0111e371de80e69f27bcfae2cb9675b982b535
Tanvir <zishantanvir05@gmail.com>
2024-09-02 11:19:04 +0600

    multiplication table of 9

--- for/multiplication-table.js
+++ for/multiplication-table.js
@@ -1,6 +1,6 @@
 //  Multiplication table for number - 9 
 let multiplication = 9;
-for (let i = 1; i <=9 ; i++){
+for (let i = 1; i <=10 ; i++){
    table = multiplication * i;
    console.log(`"Multiplication of 9 * ${i} = ${table}`)
 }
