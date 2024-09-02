338d12adb81449fa15cb89e3a7c684bdf5dacbb5
Tanvir <zishantanvir05@gmail.com>
2024-09-02 10:59:37 +0600

    add even and odd number find programm

---
+++ for/odd-even.js
@@ -0,0 +1,9 @@
+for(let i = 61; i<=100; i= i + 2){
+    console.log("Odd num - ", i)
+}
+
+for (let even = 78; even<= 98; even++){
+    if(even % 2 === 0){
+        console.log("even num - ", even);
+    }
+}
